1320c90f62ac52c7c587b82ed199c254e21992b0
Paresh Dudhat <dudhatparesh@gmail.com>
2020-06-08 14:25:04 +0530

    Updated the code with androidx libraries and latest sdks

--- app/src/main/java/saulmm/myapplication/AvatarImageBehavior.java
+++ app/src/main/java/saulmm/myapplication/AvatarImageBehavior.java
@@ -2,10 +2,11 @@ package saulmm.myapplication;
 
 import android.content.Context;
 import android.content.res.TypedArray;
-import android.support.design.widget.CoordinatorLayout;
-import android.support.v7.widget.Toolbar;
 import android.util.AttributeSet;
 import android.view.View;
+
+import androidx.appcompat.widget.Toolbar;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
